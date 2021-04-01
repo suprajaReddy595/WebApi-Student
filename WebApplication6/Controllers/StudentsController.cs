7c48499cfd34daf120ca5de69a826c4d99a5d921
Supraja C <c.supraja595@gmail.com>
2021-04-01 14:04:49 +0530

    Add project files.

---
+++ WebApplication6/Controllers/StudentsController.cs
@@ -0,0 +1,86 @@
+﻿using WebApplication6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication6.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        static List<Student> _studentList = null;
+        void Initialize()
+        {
+            _studentList = new List<Student>()
+           {
+               new Student() { StudentId=1, Name="Ajay" , Batch="B001", Marks=89, DateOfBirth=Convert.ToDateTime("12/12/2020")},
+
+               new Student() { StudentId=2, Name="Deepak" , Batch="B002", Marks=78, DateOfBirth=Convert.ToDateTime("10/06/2020")},
+           };
+
+        }
+        public StudentsController()
+        {
+            if (_studentList == null)
+            {
+                Initialize();
+            }
+        }
+        // GET: api/Students
+        public IEnumerable<Student> Get()
+        {
+            return _studentList;
+        }
+
+        // GET: api/Students/5
+        public Student Get(int id)
+        {
+            Student student = _studentList.Where(x => x.StudentId == id).FirstOrDefault();
+            return student;
+        }
+
+        // POST: api/Students
+        public void Post(Student student)
+        {
+            if (student != null)
+            {
+                _studentList.Add(student);
+            }
+
+        }
+
+        // PUT: api/Students/5
+        public void Put(int id, Student objStudent)
+        {
+            Student student = _studentList.Where(x => x.StudentId == id).FirstOrDefault();
+
+            if (student != null)
+            {
+                foreach (Student temp in _studentList)
+                {
+                    if (temp.StudentId == id)
+                    {
+                        temp.Name = objStudent.Name;
+                        temp.DateOfBirth = objStudent.DateOfBirth;
+                        temp.Batch = objStudent.Batch;
+                        temp.Marks = objStudent.Marks;
+                    }
+                }
+            }
+
+        }
+
+        // DELETE: api/Students/5
+        public void Delete(int id)
+        {
+            Student student = _studentList.Where(x => x.StudentId == id).FirstOrDefault();
+
+            if (student != null)
+            {
+                _studentList.Remove(student);
+            }
+        }
+    }
+}
---
+++ WebApplication6/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication6.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public String Batch { get; set; }
+        public int? Marks { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+    }
+}
